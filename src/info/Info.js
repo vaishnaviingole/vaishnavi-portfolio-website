8f9eb3335607d5f0f3322de90332ffb110b32d46
Vaishnavi Ingole <vaishnavingole54@gmail.com>
2025-03-08 21:08:24 +0530

    updting linkedin

--- src/info/Info.js
+++ src/info/Info.js
@@ -56,7 +56,7 @@ export const info = {
             label: 'github'
         },
         {
-            link: "https://www.linkedin.com/in/vaishnavi-ingole/",
+            link: "https://www.linkedin.com/in/vaishnaviingole/",
             icon: "fa fa-linkedin",
             label: 'linkedin'
         },{
